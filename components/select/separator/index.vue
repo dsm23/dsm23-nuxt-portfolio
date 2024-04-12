824cebf23c8814cfb3b085a5c829f0d807872559
David Murdoch <dsmurdoch357@gmail.com>
2024-04-12 13:56:16 +0100

    feat(select): add select in simple form

---
+++ components/select/separator/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import { computed } from "vue";
+  import type { HTMLAttributes } from "vue";
+  import { SelectSeparator } from "radix-vue";
+  import type { SelectSeparatorProps } from "radix-vue";
+  import cn from "~/utils/class-names";
+
+  const props = defineProps<
+    SelectSeparatorProps & { class?: HTMLAttributes["class"] }
+  >();
+
+  const delegatedProps = computed(() => {
+    const { class: _, ...delegated } = props;
+
+    return delegated;
+  });
+</script>
+
+<template>
+  <SelectSeparator
+    v-bind="delegatedProps"
+    :class="cn('-mx-1 my-1 h-px bg-muted', props.class)"
+  />
+</template>
